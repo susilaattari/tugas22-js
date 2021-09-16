229c7fec702d6ab4600862bb49dc81d04e547d38
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-16 07:31:50 +0700

    tugas 22 javasript niomic

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function perulanganForEach(){
+    let kalimatString = 'Saya ingin belajar bersama';
+    let kalimatArray = kalimatString.split(' ');
+    console.log(kalimatArray)
+    kalimatArray.forEach(function(item,index){
+        console.log(`Item : ${item} index ke ${index}`);
+    })
+}
+perulanganForEach()
